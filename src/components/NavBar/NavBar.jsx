f73c0632d3fad29f15cd8cd5907d8f546b9c8e56
Shyam <shyamoffical@gmail.com>
2023-02-06 09:30:42 +0530

    logout done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,13 @@
+import Cookies from "js-cookie"
 import React from "react"
 import { useContext } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { Icon, Label, Menu, Radio } from "semantic-ui-react"
 import ThemeContext from "../../context/ThemeContext"
 import "./NavBar.css"
 const NavBar = Props => {
   const isDarkTheme = useContext(ThemeContext)
+  const navigate = useNavigate()
   return (
     <Menu fixed="top" size={"tiny"} inverted={isDarkTheme}>
       <Menu.Menu position="right">
@@ -24,9 +26,17 @@ const NavBar = Props => {
           </Label>
         </Menu.Item>
         <Menu.Item>
-          <Icon name="sun" inverted={isDarkTheme}></Icon>
+          <Icon name="sun" size="large" inverted={isDarkTheme}></Icon>
           <Radio toggle onClick={Props.toggleDarkTheme}></Radio>
-          <Icon name="moon" inverted={isDarkTheme}></Icon>
+          <Icon name="moon" size="large" inverted={isDarkTheme}></Icon>
+        </Menu.Item>
+        <Menu.Item
+          onClick={() => {
+            Cookies.set("isLoggedIn", false, { expires: 7 })
+            navigate("/login")
+          }}
+        >
+          <Icon name="sign-out" size="large" inverted={isDarkTheme}></Icon>
         </Menu.Item>
       </Menu.Menu>
     </Menu>
